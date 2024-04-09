abe2e144746adbd514251e522b28b8ddfbffb634
Sharhab <muktarsharhadev@gmail.com>
2024-04-09 16:47:00 +0100

    style

--- client/sdata/src/layout/MainLayout/index.js
+++ client/sdata/src/layout/MainLayout/index.js
@@ -19,48 +19,51 @@ import Customization from '../Customization';
 import Header from './Header';
 
 // styles
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
-   ...theme.typography.mainContent,
-  ...(!open? {
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        }),
-        [theme.breakpoints.up('md')]: {
-            marginLeft: -(drawerWidth - 20),
-            width: `calc(100% - ${drawerWidth}px)`
-        },
-        [theme.breakpoints.down('md')]: {
-            marginLeft: '20px',
-            width: `calc(100% - ${drawerWidth}px)`,
-            padding: '16px'
-        },
-        [theme.breakpoints.down('sm')]: {
-            marginLeft: '10px',
-            width: `calc(100% - ${drawerWidth}px)`,
-            padding: '16px',
-            marginRight: '10px'
-        }
+
+const Main = styled('main', {
+  shouldForwardProp: (prop) => prop !== 'open'
+})(({ theme, open }) => ({
+  ...theme.typography.mainContent,
+  ...(!open ? {
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
     }),
-  ...(open? {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen
-        }),
-        marginLeft: 0,
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
-        width: `calc(100% - ${drawerWidth}px)`,
-        [theme.breakpoints.down('md')]: {
-            marginLeft: '20px'
-        },
-        [theme.breakpoints.down('sm')]: {
-            marginLeft: '10px'
-        }
-    })
+    [theme.breakpoints.up('md')]: {
+      marginLeft: -(drawerWidth - 20),
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+    [theme.breakpoints.down('md')]: {
+      marginLeft: '20px',
+      width: `calc(100% - ${drawerWidth}px)`,
+      padding: '16px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: '10px',
+      width: `calc(100% - ${drawerWidth}px)`,
+      padding: '16px',
+      marginRight: '10px',
+    },
+  }) : {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    width: `calc(100% - ${drawerWidth}px)`,
+    [theme.breakpoints.down('md')]: {
+      marginLeft: '20px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: '10px',
+    },
+  })
 }));
+
 
 // ==============================|| MAIN LAYOUT ||============================== //
 
