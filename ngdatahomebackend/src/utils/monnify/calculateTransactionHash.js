d1ce978b3646c4cafb99b6032ee83a4334d4cc4a
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-04-19 18:44:28 +0000

    mes

--- ngdatahomebackend/src/utils/monnify/calculateTransactionHash.js
+++
@@ -1,9 +0,0 @@
-
-const sha512 = require("js-sha512").sha512;
-
-const DEFAULT_MERCHANT_CLIENT_SECRET = process.env.MONNIFY_SECRET_KEY;
-module.exports = async (requestBody) => {
-  const result = sha512.hmac(DEFAULT_MERCHANT_CLIENT_SECRET, requestBody);
-  console.log("Computed hash", result);
-  return result;
-};
