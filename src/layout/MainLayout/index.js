d784d969aefc0c4b80f5bea579594b84e4a58665
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-04-29 18:02:33 +0100

    Update index.js

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -83,7 +83,7 @@ const MainLayout = () => {
     useEffect(() => {
         dispatch({ type: SET_MENU, opened: !matchDownMd });
      
-    }, []);
+    }, [matchDownMd]);
 
     return (
         <Box sx={{ display: 'flex' }}>
