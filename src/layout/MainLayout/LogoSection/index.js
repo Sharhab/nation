6d7477e27939a4ff29b53f9fd4ca410371545dc2
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-04-29 18:43:05 +0100

    Update index.js

--- src/layout/MainLayout/LogoSection/index.js
+++ src/layout/MainLayout/LogoSection/index.js
@@ -3,7 +3,7 @@ import { ButtonBase, Typography } from '@mui/material';
 // project imports
 import config from '../../../../src/config';
 import { Link } from 'react-router-dom';
-import Logo from '../../../ui-component/Logo';
+//import Logo from '../../../ui-component/Logo';
 
 
 
@@ -12,8 +12,9 @@ import Logo from '../../../ui-component/Logo';
 const LogoSection = () => (
     <>
         <ButtonBase disableRipple component={Link} to={config.defaultPath}>
-            <Logo />
-           {/* <Typography variant="h3">GBrain Ventures</Typography>  */}
+            
+           {/* <Logo /> */}
+           <Typography variant="h3">SHARHADATA</Typography>  
         </ButtonBase>        
     </>
 );
