c3ecfaca172ae92b54cb455a34df7e49fd738675
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-01 09:05:33 +0100

    Update index.js

--- src/network/index.js
+++ src/network/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://globstand-backend.onrender.com/api';
 
 export const RequestMethod = {
   POST: 'POST',
