f065bd482b1f2575b882b50f68f04a055402e1ba
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-09 14:07:03 +0100

    Update ProtectedRout.js

--- src/routes/ProtectedRout.js
+++ src/routes/ProtectedRout.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Cookies from 'cookies-js
+
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }) => {
-    return Cookies.get("user") ? children : <Navigate to="/pages/login" replace />;
+const ProtectedRoute = ({ children, isAuthenticated }) => {
+    return isAuthenticated ? children : <Navigate to="/pages/login" replace />;
 };
 
 module.exports = ProtectedRoute
