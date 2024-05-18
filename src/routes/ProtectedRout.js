a8e66698752b842e31f1ff672e431a52cbea4d97
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-18 09:41:54 +0100

    Update ProtectedRout.js

--- src/routes/ProtectedRout.js
+++ src/routes/ProtectedRout.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children, isLoggedIn }) => {
-    // Redirect to login page if not logged in
-    return isLoggedIn ? children : <Navigate to="/pages/login" replace />;
+return isLoggedIn ? children : <Navigate to="/pages/login" replace />;
 };
 
 export default ProtectedRoute;
