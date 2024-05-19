0f54498cc976157b848436b960f8a40232f38357
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-19 18:55:15 +0100

    Update index.js

--- src/views/data/index.js
+++ src/views/data/index.js
@@ -68,7 +68,7 @@ const BuyData = ({ title, network, sme, sme_2, mtn_cg, coup, cg }) => {
     const { enqueueSnackbar } = useSnackbar();
     const [showAlert, setshowAlert] = useState(false);
     const [showErrorAlert, setshowErrorAlert] = useState(false);
-    const navigate = useNavigate();
+
     const pinRef = useRef('');
     useEffect(() => {
           
