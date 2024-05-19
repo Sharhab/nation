f8aa30611cbb436db495b40399f5c2c564398551
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-19 13:21:21 +0100

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -667,7 +667,7 @@ export const LogoutAction = () => async (dispatch) => {
     }
 };
 
-export const LoginAction = ({ user, navigate, enqueueSnackbar }) => async (dispatch) => {
+export const loginAction = ({ user, navigate, enqueueSnackbar }) => async (dispatch) => {
     try {
         // Dispatch action to indicate the start of the login request
         dispatch({
