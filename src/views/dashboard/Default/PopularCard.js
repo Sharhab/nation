ac8e03b5f944d448d7342d3e93d2ebcfeace4c61
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-20 06:17:32 +0100

    Update PopularCard.js

--- src/views/dashboard/Default/PopularCard.js
+++ src/views/dashboard/Default/PopularCard.js
@@ -16,22 +16,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { userTransactionStatByDate } from '../../../store/actions';
 
-// ==============================|| DASHBOARD DEFAULT - POPULAR CARD ||============================== //
-
 const PopularCard = ({ isLoading }) => {
     const { userStatByDate } = useSelector((state) => state);
     const { stat, loading } = userStatByDate;
-    // const theme = useTheme();
-
-    // const [anchorEl, setAnchorEl] = useState(null);
-
-    // const handleClick = (event) => {
-    //     setAnchorEl(event.currentTarget);
-    // };
-
-    // const handleClose = () => {
-    //     setAnchorEl(null);
-    // };
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -54,48 +41,13 @@ const PopularCard = ({ isLoading }) => {
                                     <Grid item>
                                         <Typography variant="h4">FETCH TRANSACTION STATISTICS</Typography>
                                     </Grid>
-                                    {/* <Grid item>
-                                        <MoreHorizOutlinedIcon
-                                            fontSize="small"
-                                            sx={{
-                                                color: theme.palette.primary[200],
-                                                cursor: 'pointer'
-                                            }}
-                                            aria-controls="menu-popular-card"
-                                            aria-haspopup="true"
-                                            onClick={handleClick}
-                                        />
-                                        <Menu
-                                            id="menu-popular-card"
-                                            anchorEl={anchorEl}
-                                            keepMounted
-                                            open={Boolean(anchorEl)}
-                                            onClose={handleClose}
-                                            variant="selectedMenu"
-                                            anchorOrigin={{
-                                                vertical: 'bottom',
-                                                horizontal: 'right'
-                                            }}
-                                            transformOrigin={{
-                                                vertical: 'top',
-                                                horizontal: 'right'
-                                            }}
-                                        >
-                                            <MenuItem onClick={handleClose}> Today</MenuItem>
-                                            <MenuItem onClick={handleClose}> This Month</MenuItem>
-                                            <MenuItem onClick={handleClose}> This Year </MenuItem>
-                                        </Menu>
-                                    </Grid> */}
                                 </Grid>
                             </Grid>
                             <Grid item xs={12} sx={{ pt: '16px !important' }}>
-                                {/* <Paper variant="elevation" elevation={3} sx={{ mt: 2, padding: '10px' }}> */}
                                 <Typography variant="body1" sx={{ fontSize: '1rem', mb: 2, fontWeight: 500 }}>
                                     Choose Date
                                 </Typography>
-                                <div
-                                    style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
-                                >
+                                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
                                     <DatePicker
                                         format="DD-MM-YYYY"
                                         value={value}
@@ -106,125 +58,38 @@ const PopularCard = ({ isLoading }) => {
                                         Fetch
                                     </Button>
                                 </div>
-                                {/* </Paper> */}
                             </Grid>
                             {Object.keys(stat).length !== 0 && (
                                 <Grid item xs={12}>
                                     <Grid container direction="column">
-                                        <Grid item>
-                                            <Grid container alignItems="center" justifyContent="space-between">
-                                                <Grid item>
-                                                    <Typography variant="subtitle1" color="inherit">
-                                                        Total data purcahse
-                                                    </Typography>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Grid container alignItems="center" justifyContent="space-between">
-                                                        <Grid item>
-                                                            <Typography variant="subtitle1" color="inherit">
-                                                                ₦{stat?.totaldatas}
-                                                            </Typography>
-                                                        </Grid>
+                                        {[
+                                            { label: 'Total data purchase', value: stat?.totaldatas },
+                                            { label: 'Total Funding', value: stat?.totalfundings },
+                                            { label: 'Total Airtime', value: stat?.totalairtimes },
+                                            { label: 'Total Cable', value: stat?.totalcables },
+                                            { label: 'Total Electricity', value: stat?.totalelelectricity }
+                                        ].map((item, index) => (
+                                            <div key={index}>
+                                                <Grid container alignItems="center" justifyContent="space-between">
+                                                    <Grid item>
+                                                        <Typography variant="subtitle1" color="inherit">
+                                                            {item.label}
+                                                        </Typography>
+                                                    </Grid>
+                                                    <Grid item>
+                                                        <Typography variant="subtitle1" color="inherit">
+                                                            ₦{item.value}
+                                                        </Typography>
                                                     </Grid>
                                                 </Grid>
-                                            </Grid>
-                                        </Grid>
+                                                {index < 4 && <Divider sx={{ my: 1.5 }} />}
+                                            </div>
+                                        ))}
                                     </Grid>
-                                    <Divider sx={{ my: 1.5 }} />
-                                    <Grid container direction="column">
-                                        <Grid item>
-                                            <Grid container alignItems="center" justifyContent="space-between">
-                                                <Grid item>
-                                                    <Typography variant="subtitle1" color="inherit">
-                                                        Total Funding
-                                                    </Typography>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Grid container alignItems="center" justifyContent="space-between">
-                                                        <Grid item>
-                                                            <Typography variant="subtitle1" color="inherit">
-                                                                ₦{stat?.totalfundings}
-                                                            </Typography>
-                                                        </Grid>
-                                                    </Grid>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                    <Divider sx={{ my: 1.5 }} />
-                                    <Grid container direction="column">
-                                        <Grid item>
-                                            <Grid container alignItems="center" justifyContent="space-between">
-                                                <Grid item>
-                                                    <Typography variant="subtitle1" color="inherit">
-                                                        Total Airtime
-                                                    </Typography>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Grid container alignItems="center" justifyContent="space-between">
-                                                        <Grid item>
-                                                            <Typography variant="subtitle1" color="inherit">
-                                                                ₦{stat?.totalairtimes}
-                                                            </Typography>
-                                                        </Grid>
-                                                    </Grid>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                    <Divider sx={{ my: 1.5 }} />
-                                    <Grid container direction="column">
-                                        <Grid item>
-                                            <Grid container alignItems="center" justifyContent="space-between">
-                                                <Grid item>
-                                                    <Typography variant="subtitle1" color="inherit">
-                                                        Total Cable
-                                                    </Typography>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Grid container alignItems="center" justifyContent="space-between">
-                                                        <Grid item>
-                                                            <Typography variant="subtitle1" color="inherit">
-                                                                ₦{stat?.totalcables}
-                                                            </Typography>
-                                                        </Grid>
-                                                    </Grid>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                    <Divider sx={{ my: 1.5 }} />
-                                    <Grid container direction="column">
-                                        <Grid item>
-                                            <Grid container alignItems="center" justifyContent="space-between">
-                                                <Grid item>
-                                                    <Typography variant="subtitle1" color="inherit">
-                                                        Total Electricity
-                                                    </Typography>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Grid container alignItems="center" justifyContent="space-between">
-                                                        <Grid item>
-                                                            <Typography variant="subtitle1" color="inherit">
-                                                                ₦{stat?.totalelelectricity}
-                                                            </Typography>
-                                                        </Grid>
-                                                    </Grid>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                    <Divider sx={{ my: 1.5 }} />
                                 </Grid>
                             )}
                         </Grid>
                     </CardContent>
-                    {/* <CardActions sx={{ p: 1.25, pt: 0, justifyContent: 'center' }}>
-                        <Button size="small" disableElevation>
-                            View All
-                            <ChevronRightOutlinedIcon />
-                        </Button>
-                    </CardActions> */}
                 </MainCard>
             )}
         </>
