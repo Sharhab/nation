d67b3baf4de33e1c8f2b254e1cfff902edcb11c8
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-20 07:06:04 +0100

    Update ProtectedRout.js

--- src/routes/ProtectedRout.js
+++ src/routes/ProtectedRout.js
@@ -1,10 +1,11 @@
+// ProtectedRoute.js
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = useSelector(state => state.userStat.isLoggedIn);
-  return isLoggedIn ? children : <Navigate to="/pages/login" />;
+    const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+    return isLoggedIn ? children : <Navigate to="/pages/login" />;
 };
 
 export default ProtectedRoute;
