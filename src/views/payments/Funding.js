3be7f1f525d619e940bcf6a87b43d8b8b4125ddc
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-20 10:32:44 +0100

    Update Funding.js

--- src/views/payments/Funding.js
+++ src/views/payments/Funding.js
@@ -24,7 +24,7 @@ const Funding = () => {
 
     useEffect(() => {
       if(!isLoggedIn){
-          navigate ('/pages/login')
+          navigate('/pages/login')
       }
         dispatch(userAction({ navigate }));
     }, [navigate, dispatch, isLoggedIn]);
