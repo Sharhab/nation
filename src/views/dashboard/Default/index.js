98a5de30094b5cc200e81f0424a123a83cd73b27
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-23 16:18:59 +0100

    Update index.js

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,40 +1,36 @@
-// material-ui
 import { Grid } from '@mui/material';
-
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { getNotificationDetails, userAction } from '../../../store/actions';
 import { gridSpacing } from '../../../store/constant';
-// project imports
 import EarningCard from './EarningCard';
 import ProductListing from './ProductListing';
 import { useSnackbar } from 'notistack';
 import FeedBack from '../../feedBack';
 import PopularCard from './PopularCard';
+import AccountDetails from '../../payment/AccountDetail'; // Import the AccountDetails component
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
 const Dashboard = () => {
     const [isLoading, setLoading] = useState(true);
-    const { notificationDetails, login } = useSelector((state) => state);
+    const { notificationDetails, login, loggedInUser } = useSelector((state) => state); // Add loggedInUser to state selection
     const { notification } = notificationDetails;
     const { isLoggedIn } = login;
+    const { user } = loggedInUser; // Extract user from loggedInUser
     const { enqueueSnackbar } = useSnackbar();
     const [showAlert, setshowAlert] = useState(false);
-    // const [ setshowErrorAlert] = useState(false);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     useEffect(() => {
         setLoading(false);
-    if(!isLoggedIn) {
-        navigate('/pages/login');
-    }
-        
+        if (!isLoggedIn) {
+            navigate('/pages/login');
+        }
+
         dispatch(userAction({ navigate }));
-       // dispatch(userTransactionStat({ navigate }));
-        // dispatch(userTransactionStatByDate({ navigate }));
         dispatch(getNotificationDetails({ enqueueSnackbar, setshowAlert }));
     }, [dispatch, navigate, isLoggedIn, enqueueSnackbar]);
 
@@ -47,21 +43,14 @@ const Dashboard = () => {
                         <br />
                         <EarningCard isLoading={isLoading} message={notification?.message} />
                     </Grid>
-                    {/* <Grid item lg={4} md={6} sm={6} xs={12}>
-                        <TotalOrderLineChartCard isLoading={isLoading} />
-                    </Grid>
-                    <Grid item lg={4} md={12} sm={12} xs={12}>
-                        <Grid container spacing={gridSpacing}>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                <TotalIncomeDarkCard isLoading={isLoading} />
-                            </Grid>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                <TotalIncomeLightCard isLoading={isLoading} />
-                            </Grid>
-                        </Grid>
-                    </Grid> */}
+                    {/* Other grid items can be here */}
                 </Grid>
             </Grid>
+            {user?.hasAccountNum && (
+                <Grid item xs={12}>
+                    <AccountDetails accounts={user.monnify_bank_details || []} /> {/* Integrate AccountDetails here */}
+                </Grid>
+            )}
             <Grid item xs={12}>
                 <Grid container spacing={gridSpacing}>
                     <Grid item xs={12} md={8}>
