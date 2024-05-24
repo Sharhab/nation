4b744a107ecf47b54a17940019cfe51eedcfe244
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-24 06:53:37 +0100

    Create BvnVerificationStatus.js

---
+++ src/views/payments/BvnVerificationStatus.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
+import VerifiedIcon from '@mui/icons-material/Verified'; // You can use any appropriate icon
+
+const BvnVerificationStatus = ({ user }) => {
+    if (!user?.hasAccountNum) {
+        return null;
+    }
+
+    return (
+        <div>
+            {user.updateBvn ? (
+                <Typography variant="subtitle1" color="primary" display="flex" alignItems="center">
+                    <VerifiedIcon style={{ marginRight: '8px' }} />
+                    BVN Verified
+                </Typography>
+            ) : (
+                <Typography variant="subtitle1">
+                    <Link to="/verify_account" style={{ textDecoration: 'none', color: 'red' }}>
+                        Verify your BVN. This is required by CBN to secure your account.
+                    </Link>
+                </Typography>
+            )}
+        </div>
+    );
+};
+
+export default BvnVerificationStatus;
