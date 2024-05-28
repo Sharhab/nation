1a92e3850a87145b1e6e34a21e9689247945fbaf
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-28 08:52:45 +0100

    Update BvnVerificationStat.js

--- src/views/payments/BvnVerificationStat.js
+++ src/views/payments/BvnVerificationStat.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import VerifiedIcon from '@mui/icons-material/Verified'; // You can use any appropriate icon
 
 const BvnVerificationStatus = ({ user }) => {
-    if (!user?.hasAccountNum || !user?.updateBvn) {
+    if (!user?.hasAccountNum) {
         return null;
     }
 
     return (
-        <div>
+        <Box textAlign="center">
             {user.updateBvn ? (
-                <Typography variant="subtitle1" color="primary" display="flex" alignItems="center">
-                   <Typography variant="h5"> Virtual Account Update:</Typography>
-         <Typography variant="h4"> 
-                   Status: <VerifiedIcon style={{ marginRight: '8px' alignItmes: "center" justifyContent: "center"}} />
-           </Typography>
-                </Typography>
+                <>
+                    <Typography variant="h5" gutterBottom>
+                        Virtual Account Update:
+                    </Typography>
+                    <Box display="flex" justifyContent="center" alignItems="center">
+                        <Typography variant="subtitle1" color="primary" display="flex" alignItems="center">
+                            Status: 
+                            <VerifiedIcon style={{ marginLeft: '8px' }} />
+                        </Typography>
+                    </Box>
+                </>
             ) : (
                 <Typography variant="subtitle1">
                     <Link to="/verifyBvn" style={{ textDecoration: 'none', color: 'red' }}>
                         To update your virtual accounts, as required
-                         by the CBN CLICK HERE
+                        by the CBN CLICK HERE
                     </Link>
                 </Typography>
             )}
-        </div>
+        </Box>
     );
 };
 
