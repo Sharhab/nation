3b40aafce996bb30fba4ecb25a7a80270c38083f
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-05-30 11:59:12 +0100

    Update index.js

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -95,7 +95,7 @@ const MainLayout = () => {
                 color="inherit"
                 elevation={0}
                 sx={{
-                    bgcolor: theme.palette.background.default,
+                    
                     transition: leftDrawerOpened ? theme.transitions.create('width') : 'none'
                 }}
             >
