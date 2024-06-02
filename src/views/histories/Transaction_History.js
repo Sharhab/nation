aa9245866c69a0ba3bf33a25b14804a1c4a961b5
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-06-02 05:54:29 +0100

    Update Transaction_History.js

--- src/views/histories/Transaction_History.js
+++ src/views/histories/Transaction_History.js
@@ -1,64 +1,164 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getHistories } from '../../store/actions'; // Ensure the correct import path
+// material-ui
+// project imports
 import { Box, CircularProgress } from '@mui/material';
+import Cookies from 'js-cookie';
 import moment from 'moment';
 import MUIDataTable from 'mui-datatables';
 import { useSnackbar } from 'notistack';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
-import MainCard from '../../ui-component/cards/MainCard'; // Ensure the correct import path
+import { getHistories, userAction } from '../../store/actions';
+import MainCard from '../../ui-component/cards/MainCard';
+
+// ==============================|| SAMPLE PAGE ||============================== //
 
 const Histories = () => {
     const { transactionHistory } = useSelector((state) => state);
-    const { loading, orders } = transactionHistory;
+    const { loading, histories } = transactionHistory;
 
     const { enqueueSnackbar } = useSnackbar();
     const navigate = useNavigate();
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getHistories());
-    }, [dispatch]);
+        !Cookies.get('user') && navigate('/pages/login');
+        dispatch(userAction({ navigate }));
+        dispatch(getHistories({ enqueueSnackbar }));
+    }, [dispatch, enqueueSnackbar, navigate]);
 
     const columns = [
-        { name: 'name', label: 'Transaction Name', options: { filter: true, sort: true } },
-        { name: 'status', label: 'Status', options: { filter: true, sort: false } },
-        { name: 'date', label: 'Date', options: { filter: true, sort: true } },
-        { name: 'amount', label: 'Amount', options: { filter: true, sort: false } },
-        { name: 'plan', label: 'Plan', options: { filter: true, sort: false } },
-        { name: 'previous_balance', label: 'Previous Balance', options: { filter: true, sort: false } },
-        { name: 'current_balance', label: 'Current Balance', options: { filter: true, sort: false } },
-        { name: 'beneficiary', label: 'Beneficiary', options: { filter: true, sort: false } },
-        { name: 'network', label: 'Network', options: { filter: true, sort: false } },
-        { name: 'exam_pin', label: 'Serial No / Pin', options: { filter: true, sort: false } },
-        { name: 'electricity', label: 'Token', options: { filter: true, sort: false } },
-        { name: 'ref', label: 'Reference', options: { filter: true, sort: false } }
+        {
+            name: 'name',
+            label: 'Transaction Name',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'status',
+            label: 'Status',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+
+        {
+            name: 'date',
+            label: 'Date',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'amount',
+            label: 'Amount',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+        {
+            name: 'plan',
+            label: 'Plan',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+        {
+            name: 'previous_balance',
+            label: 'Previous Balance',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+        {
+            name: 'current_balance',
+            label: 'Current Balance',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+        {
+            name: 'beneficiary',
+            label: 'Beneficiary',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+        {
+            name: 'network',
+            label: 'Network',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+
+        {
+            name: 'exam_pin',
+            label: 'Serial No / Pin',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+        {
+            name: 'electricity',
+            label: 'Token',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+
+        {
+            name: 'ref',
+            label: 'Refrence',
+            options: {
+                filter: true,
+                sort: false
+            }
+        }
     ];
+    histories.histories?.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
-    const data = orders.map((order) => {
-        const formattedDate = moment(order.createdAt).format('LLLL');
-
+    const data = histories.histories?.map((serv, i) => {
+        // const strDate = new Date(serv?.createdAt);
+        const formattedDate = moment(serv?.createdAt).format('LLLL');
+        // function convert(strDate) {
+        
         return {
-            id: order._id,
-            name: order.TRX_Name || order.plan || '-',
-            ref: order.request_id || order.ref || order.tx_ref || order.request_Id || '-',
-            amount: order.amount,
-            previous_balance: order.previous_balance,
-            current_balance: order.current_balance,
-            network: order.network || '-',
-            beneficiary: order.beneficiary || order.phone_number || order.phone || '-',
-            status: order.status,
-            exam_pin: order.purchased_pin || '-',
-            electricity: order.purchased_token || '-',
+            id: `${serv._id}`,
+            name: `${serv.TRX_Name ? serv?.TRX_Name : serv?.plan || '-'}`,
+            ref: `${serv.request_id || serv?.ref || serv?.tx_ref || serv?.request_Id || '-'}`,
+            amount: `${serv.amount}`,
+            previous_balance: `${serv?.previous_balance}`,
+            current_balance: `${serv?.current_balance}`,
+            network: `${serv.network ? serv.network : '-'}`,
+            beneficiary: `${serv.beneficiary ? serv?.beneficiary : serv?.phone_number || serv?.phone || '-'}`,
+            status: `${serv.status}`,
+            exam_pin: `${serv.purchased_pin || '-'}`,
+            electricity: `${serv.purchased_token || '-'}`,
             date: formattedDate,
-            plan: order.plan || '-'
+            plan: `${serv?.plan || serv?.Plan || '-'}`
         };
     });
 
     const options = {
         filterType: 'checkbox',
         selectableRows: 'none'
+        // sortOrder: {
+        //     name: 'date',
+        //     direction: 'desc'
+        // }
     };
-
     return (
         <MainCard title={'Transactions History'}>
             {loading ? (
